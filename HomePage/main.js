2f6d27e5c756dc570f5e1f5a8a49505ffab342ad
Negar Karimi <negar.skills@gmail.com>
2025-12-03 04:55:31 +0000

    testimonials-section step2

--- HomePage/main.js
+++ HomePage/main.js
@@ -8,3 +8,37 @@ menuOpenButton.addEventListener("click", function () {
 menuCloseButton.addEventListener("click", function () {
   menuOpenButton.click();
 });
+
+// Initialize Swiper
+const swiper = new Swiper(".slider-wrapper", {
+  // Optional parameters
+  // direction: "vertical",
+  spaceBetween: 25,
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    daynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  //Responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: { slidesPerView: 2 },
+    1024: { slidesPerView: 4 },
+  },
+  // // And if we need scrollbar
+  // scrollbar: {
+  //   el: ".swiper-scrollbar",
+  // },
+});
